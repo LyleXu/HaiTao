c59f33ec87b7a53a5f6a8f5c407fa448560150b9
Lyle <chanxl009@gmail.com>
2014-11-02 10:23:52 +0800

    Test Login with Weibo_QQ

---
+++ HaiTao/MainViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HaiTao
+//
+//  Created by gtcc on 11/2/14.
+//  Copyright (c) 2014 home. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
