7e7064a2799d5b1417508239ac489d48748ed818
Lyle <chanxl009@gmail.com>
2014-11-14 11:12:48 +0800

    message

---
+++ HaiTao/Message/cell/ChartCellFrame.h
@@ -0,0 +1,18 @@
+//
+//  ChartCellFrame.h
+//  气泡
+//
+//  Created by zzy on 14-5-13.
+//  Copyright (c) 2014年 zzy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreGraphics/CoreGraphics.h>
+#import "ChartMessage.h"
+#import <UIKit/UIKit.h>
+@interface ChartCellFrame : NSObject
+@property (nonatomic,assign) CGRect iconRect;
+@property (nonatomic,assign) CGRect chartViewRect;
+@property (nonatomic,strong) ChartMessage *chartMessage;
+@property (nonatomic, assign) CGFloat cellHeight; //cell高度
+@end
---
+++ HaiTao/MessageViewController.h
@@ -0,0 +1,13 @@
+//
+//  MessageViewController.h
+//  HaiTao
+//
+//  Created by gtcc on 11/14/14.
+//  Copyright (c) 2014 home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "HYSegmentedControl.h"
+@interface MessageViewController : UIViewController
+@property (retain, nonatomic)  HYSegmentedControl *segmentedControl;
+@end
